53872b83daa1fed274eec76103dfd17553f290c0
Ritwik Srivastava <ritwikdbz@gmail.com>
2022-10-25 06:09:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Marvel Movie Character Creator")
+print()
+print("answer in Yes or No please")
+print()
+Q1 = input("Do you like 'hanging around'")
+if Q1 == "No":
+  print("Then you are not Spider-man")
+
+Q2 = input("Do you have a 'gravelly voice'")
+if Q2 == "No":
+  print("Aww, then you are not Korg")
+
+Q3 = input("Do you often feel 'Marvelous'")
+
+if Q3 == "Yes":
+  print("Aha! You are Captain Marvel!")
+
+else:
+  print("you are not any of the marvel characters I guess")
